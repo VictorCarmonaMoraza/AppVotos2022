9e59d279a199a7267fecf573e309d7c8108d4eef
Victor <VictorCarmona9035@hotmail.com>
2022-11-09 20:18:32 +0100

    2-Creacion de paginas y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { GotyComponent } from './pages/goty/goty.component';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from './components/components.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    GotyComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
